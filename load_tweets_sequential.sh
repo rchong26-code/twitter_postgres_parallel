edbcceb0bb110a7668af22a45dbf6f13704a4720
Renee Chong <rchong26@cmc.edu>
2026-04-16 22:49:30 -0700

    Fix script paths for CI

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,21 +6,27 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
+    echo $file
     # copy your solution to the twitter_postgres assignment here
+    python3 -u load_tweets.py \
+	--db=postgresql://postgres:pass@localhost:5441/pg_denormalized \
+	--inputs $file
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
+    echo $file
     # copy your solution to the twitter_postgres assignment here
+    python3 -u load_tweets.py \
+	--db=postgresql://postgres:pass@localhost:5441/pg_normalized \
+	--inputs $file
 done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:5441/pg_normalized --inputs $file
 done
